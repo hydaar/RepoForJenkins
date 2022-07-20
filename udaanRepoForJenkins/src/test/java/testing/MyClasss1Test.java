9d52b6885c6d8d136f1822780732aa89860c9af6
user <user@GLOVE>
2022-07-20 17:44:06 +0530

    at 5:43 for jenkins type 3 pom type

---
+++ udaanRepoForJenkins/src/test/java/testing/MyClasss1Test.java
@@ -0,0 +1,53 @@
+package testing;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+
+
+
+
+
+public class MyClasss1Test {
+	
+@Test
+
+public void sample() 
+
+{
+	System.out.println("hello everybody");
+}
+	
+	
+@Test
+
+public void sample2()
+{
+	
+	String BROWSER = System.getProperty("browser");
+	String URL =System.getProperty("url");
+	
+	System.out.println("hello all");
+	System.out.println("hi all");
+	System.out.println("enjoy all");
+	System.out.println("eat  all");
+	
+	System.out.println("good bye all");
+	System.out.println("hello all");
+	System.out.println("bye all");
+	System.out.println("welcome  all");
+	
+
+}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
